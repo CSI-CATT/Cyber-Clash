2507a3f8b08ff681e6ac643fbf4f4a87cc9610ed
ergocoder <ergo.bhargavi@gmail.com>
2025-10-05 14:24:01 +0530

    updated config.example.js

---
+++ CyberClash/js/config.example.js
@@ -0,0 +1,9 @@
+// js/config.example.js
+export const firebaseConfig = {
+  apiKey: "REPLACE_ME",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id"
+};
